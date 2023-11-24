7907d2f3b440be472aaf4d1bafac4bdd55f9a62b
mdl <madalin.rusu23@gmail.com>
2023-11-24 17:03:58 +0200

    Small refactor on unit tests

--- tests/test_model_object.py
+++ tests/test_model_object.py
@@ -11,18 +11,17 @@ class TestModelObject(TestCase):
         mp = MagicMock()
         model = MagicMock()
         encoders = MagicMock()
-        self.model_object = ModelObject(mp, model, encoders)
+        auto_clear = 2
+        self.model_object = ModelObject(mp, model, encoders, auto_clear)
 
     @patch('keras.backend.clear_session')
     def test_clear_session_on_prediction(self, mock_clear_session):
         x_batch_generator = MagicMock(spec=XBatchGenerator)
+
         self.model_object.predict(x_batch_generator)
         mock_clear_session.assert_not_called()
         self.assertEqual(self.model_object._state_calls, 1)
-        self.model_object.predict(x_batch_generator)
-        mock_clear_session.assert_not_called()
-        self.assertEqual(self.model_object._state_calls, 2)
-        self.model_object._state_calls = self.model_object._state_autoclear
+
         self.model_object.predict(x_batch_generator)
         mock_clear_session.assert_called_once()
         self.assertEqual(self.model_object._state_calls, 0)
@@ -35,12 +34,11 @@ class TestModelObject(TestCase):
         x_y_batch_generator.inputs = {"data_source": data_source}
         x_y_batch_generator.targets = {"data_source": data_source}
         x_y_batch_generator.batch_size = 1
+
         self.model_object.evaluate_raw(x_y_batch_generator)
         mock_clear_session.assert_not_called()
         self.assertEqual(self.model_object._state_calls, 1)
-        self.model_object.evaluate_raw(x_y_batch_generator)
-        mock_clear_session.assert_not_called()
-        self.assertEqual(self.model_object._state_calls, 2)
+
         self.model_object._state_calls = self.model_object._state_autoclear
         self.model_object.evaluate_raw(x_y_batch_generator)
         mock_clear_session.assert_called_once()
